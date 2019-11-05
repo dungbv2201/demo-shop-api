97d22a9ace9b0309572c37091c99bfcef12bc34f
Dũng <dungbv2201@gmail.com>
2019-11-05 23:13:39 +0700

    init project

---
+++ app/Http/Controllers/Client/productController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class productController extends Controller
+{
+    public function getProducts(){
+
+    }
+}
