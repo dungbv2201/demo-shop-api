b1c7b2a67846b8c7d2331107a49095ad24ae0698
Dũng <dungbv2201@gmail.com>
2019-11-06 00:56:29 +0700

    add package cart

--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Client;
 
 use App\Models\Product;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -11,5 +12,10 @@ class ProductController extends Controller
     public function index(){
         $products = Product::with('category')->get();
         return response()->json( \App\Http\Resources\Product::collection($products));
+    }
+
+    public function addCart(){
+        Cart::add('293ad', 'Product 1', 1, 9.99, 550);
+        dd(Cart::content());
     }
 }
