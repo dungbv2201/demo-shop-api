c9a36d86e7741f092b811773f25c123854f34f93
Dũng <dungbv2201@gmail.com>
2019-11-06 00:42:52 +0700

    fix cors

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::namespace('Client')->group(function(){
+
+Route::group(['namespace'=>'Client','middleware' =>'cors'],function(){
     Route::get('products','ProductController@index');
 });
